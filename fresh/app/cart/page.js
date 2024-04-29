b5484937badec56de4b33854f2c56af1617fbd99
김도근 <dawson@300cbt.com>
2024-04-29 12:58:15 +0900

    Update routing

---
+++ fresh/app/cart/page.js
@@ -0,0 +1,9 @@
+export default function Cart() {
+    let name = 'park'
+    return (
+      <div>
+        <h4 style={{ color: 'red', textAlign: 'center' }}>장바구니입니다</h4>
+      </div>
+    );
+  }
+  
---
+++ fresh/app/cart/payment/page.js
@@ -0,0 +1,8 @@
+export default function Payment() {
+    let name = 'park'
+    return (
+      <div>
+        <h4 style={{ color: 'red', textAlign: 'center' }}>결제페이지입니다</h4>
+      </div>
+    );
+  }

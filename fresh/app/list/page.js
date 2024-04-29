581f61433b041e4de35fde9defd229f3b16a0970
김도근 <dawson@300cbt.com>
2024-04-29 13:39:31 +0900

    Add img

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function List() {
   let product = ['Tomato', 'Pasta', 'Coconut']
 
@@ -7,7 +9,8 @@ export default function List() {
       {
         product.map((a, i)=>{
           return (
-            <div className="food">
+            <div className="food" key={ i }>
+              <img src={`/food${i}.png`} className="food-img" />
               <h4>{ product[i] } $40</h4>
             </div>      
           )
